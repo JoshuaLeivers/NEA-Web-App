a4c4fad0ae214367c648040674029b77913f388c
JoshuaLeivers <joshualeivers02@gmail.com>
2019-11-14 20:12:16 +0000

    Add empty templates, add to app.py
    Added dashboard.html and login.html
    Updated app.py

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, render_template
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+app.config["MYSQL_DATABASE_USER"] = "portal_man"
+app.config["MYSQL_DATABASE_PASSWORD"] = "f8hg30DGt&4svaA4"
 
-user = {"Username": request.cookies.get("Username"),
-        "SessionID": request.cookies.get("SessionID")}
+mysql = MySQL()
+mysql.init_app(app)
+cursor = mysql.get_db().cursor()
+
+cookies = request.cookies
+user = {"Username": cookies.get("Username"),
+        "SessionID": cookies.get("SessionID")}
+
 
 @app.route("/")
 def home():
     return "This is a test!　私はジョシュアです。"
+
+
+# Utility functions
+def check_login():
+    template = "login.html"
+    sid = user["SessionID"]
+    if not sid is None:
 
 
 if __name__ == '__main__':
